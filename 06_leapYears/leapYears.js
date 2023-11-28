f12ee42804d2b9bed9ce534f0e98b011b741ea32
ilathem <isaiahlathem@gmail.com>
2023-11-28 17:30:00 -0600

    Finish 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num % 100 === 0 &&
+        num % 400 === 0) {
+        return true;
+    } else if (
+        num % 100 === 0 &&
+        !num % 400 === 0) {
+        return false;
+    } else if (
+        num % 4 === 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
