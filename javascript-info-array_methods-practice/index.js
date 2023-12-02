44b0f9d1c190a9dcdb7368b66e6001d3c4c27bb5
ilathem <isaiahlathem@gmail.com>
2023-12-01 21:53:58 -0600

    Add questions for remainder of practife

--- javascript-info-array_methods-practice/index.js
+++ javascript-info-array_methods-practice/index.js
@@ -98,7 +98,7 @@
 // let names = users.map(user => user.name);
 // console.log( names ); // John, Pete, Mary
 
-// Task 8: Map to objects
+// Task 8: Map to objects !!LEFT OFF!!!
 // You have an array of user objects, each one has name, surname and id.
 // Write the code to create another array from it, of objects with id and 
 // fullName, where fullName is generated from name and surname.
@@ -121,3 +121,80 @@ usersMapped = [
 console.table(usersMapped);
 console.log( usersMapped[0].id ) // 1
 console.log( usersMapped[0].fullName ) // John Smith
+
+// Task 9: Shuffle an array
+// Write the function shuffle(array) that shuffles (randomly reorders) 
+// elements of the array.
+// Multiple runs of shuffle may lead to different orders of elements. 
+// For instance:
+// let arr = [1, 2, 3];
+// shuffle(arr);
+// arr = [3, 2, 1]
+// shuffle(arr);
+// arr = [2, 1, 3]
+// shuffle(arr);
+// arr = [3, 1, 2]
+// ...
+
+// Task 10: Get average age.
+// Write the function getAverageAge(users) that gets an array of objects with property age and returns the average age.
+// 
+// The formula for the average is (age1 + age2 + ... + ageN) / N.
+// 
+// For instance:
+// 
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 29 };
+// 
+// let arr = [ john, pete, mary ];
+// 
+// alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+// Task 11: Filter unique array members
+// Let arr be an array.
+// 
+// Create a function unique(arr) that should return an array with unique items of arr.
+// 
+// For instance:
+// 
+// function unique(arr) {
+//   /* your code */
+// }
+// 
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+// 
+// alert( unique(strings) ); // Hare, Krishna, :-O
+
+// Task 11: Create keyed object from array
+// Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+// 
+// Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+// 
+// For example:
+// 
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20},
+//   {id: 'ann', name: "Ann Smith", age: 24},
+//   {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
+// 
+// let usersById = groupById(users);
+// 
+// /*
+// // after the call we should have:
+// 
+// usersById = {
+//   john: {id: 'john', name: "John Smith", age: 20},
+//   ann: {id: 'ann', name: "Ann Smith", age: 24},
+//   pete: {id: 'pete', name: "Pete Peterson", age: 31},
+// }
+// */
+// Such function is really handy when working with server data.
+// 
+// In this task we assume that id is unique. There may be no two array items with the same id.
+// 
+// Please use array .reduce method in the solution.
+
