1eaa92fa9d5c55f38155db2b6d27e50b5c27567d
ilathem <isaiahlathem@gmail.com>
2023-12-07 23:45:52 -0600

    Comment out task 12

--- javascript-info-array_methods-practice/index.js
+++ javascript-info-array_methods-practice/index.js
@@ -196,24 +196,24 @@
 // Let’s say we received an array of users in the form {id:..., name:..., age:... }.
 // 
 // Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
-function groupById(arr) {
-    let obj = {};
-    arr.forEach((value) => {
-        obj[value.id] = value;
-    })
-    return obj;
-}
+// function groupById(arr) {
+//     let obj = {};
+//     arr.forEach((value) => {
+//         obj[value.id] = value;
+//     })
+//     return obj;
+// }
 // 
 // For example:
 // 
-let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
-];
+// let users = [
+//   {id: 'john', name: "John Smith", age: 20},
+//   {id: 'ann', name: "Ann Smith", age: 24},
+//   {id: 'pete', name: "Pete Peterson", age: 31},
+// ];
 // 
-let usersById = groupById(users);
-console.table(usersById)
+// let usersById = groupById(users);
+// console.table(usersById)
 // 
 // 
 /*
