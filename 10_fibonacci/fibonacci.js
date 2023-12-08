67be28a905aeb845cb5ac9ebb1ab9c489d92d938
ilathem <isaiahlathem@gmail.com>
2023-12-08 15:47:07 -0600

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (typeof num === 'string') num = Number(num)
+    if (num < 0) return "OOPS";
+    const fib = [];
+    for (let i = 0; i < num; i++) {
+        if (i === 0) fib.push(1);
+        else {
+            fib.push((fib[i - 2] ?? 0) + (fib[i - 1]));
+        }
+    }
+    return fib[fib.length - 1]
 };
 
 // Do not edit below this line
