8062b39ba8ad707ed1905ef5d0844c0539dc9a55
ilathem <isaiahlathem@gmail.com>
2023-12-07 23:43:31 -0600

    Finish task 12

--- javascript-info-array_methods-practice/index.js
+++ javascript-info-array_methods-practice/index.js
@@ -178,46 +178,53 @@
 // 
 // For instance:
 // 
-function unique(arr) {
-    uniqueArray = [];
-    arr.forEach((value) => {
-        if (!uniqueArray.includes(value)) {
-            uniqueArray.push(value);
-        }
-    })
-    return uniqueArray;
-}
-// 
-let strings = ["Hare", "Krishna", "Hare", "Krishna",
-  "Krishna", "Krishna", "Hare", "Hare", ":-O"
-];
-// 
-alert( unique(strings) ); // Hare, Krishna, :-O
+// function unique(arr) {
+//     uniqueArray = [];
+//     arr.forEach((value) => {
+//         if (!uniqueArray.includes(value)) {
+//             uniqueArray.push(value);
+//         }
+//     })
+//     return uniqueArray;
+// }
+// let strings = ["Hare", "Krishna", "Hare", "Krishna",
+//   "Krishna", "Krishna", "Hare", "Hare", ":-O"
+// ];
+// alert( unique(strings) ); // Hare, Krishna, :-O
 
 // Task 11: Create keyed object from array
 // Let’s say we received an array of users in the form {id:..., name:..., age:... }.
 // 
 // Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+function groupById(arr) {
+    let obj = {};
+    arr.forEach((value) => {
+        obj[value.id] = value;
+    })
+    return obj;
+}
 // 
 // For example:
 // 
-// let users = [
-//   {id: 'john', name: "John Smith", age: 20},
-//   {id: 'ann', name: "Ann Smith", age: 24},
-//   {id: 'pete', name: "Pete Peterson", age: 31},
-// ];
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
 // 
-// let usersById = groupById(users);
+let usersById = groupById(users);
+console.table(usersById)
 // 
-// /*
-// // after the call we should have:
 // 
-// usersById = {
-//   john: {id: 'john', name: "John Smith", age: 20},
-//   ann: {id: 'ann', name: "Ann Smith", age: 24},
-//   pete: {id: 'pete', name: "Pete Peterson", age: 31},
-// }
-// */
+/*
+    after the call we should have:
+
+    usersById = {
+    john: {id: 'john', name: "John Smith", age: 20},
+    ann: {id: 'ann', name: "Ann Smith", age: 24},
+    pete: {id: 'pete', name: "Pete Peterson", age: 31},
+    }
+*/
 // Such function is really handy when working with server data.
 // 
 // In this task we assume that id is unique. There may be no two array items with the same id.
