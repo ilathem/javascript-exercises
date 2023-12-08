3898b5daee74e7bdd8b487d729f23c47f4e6652a
ilathem <isaiahlathem@gmail.com>
2023-12-08 16:27:43 -0600

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return (people.reduce((oldestPerson, currentPerson) => {
+        const oldestPersonAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+        const currentPersonAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+        if (currentPersonAge > oldestPersonAge) {
+            return currentPerson;
+        } else {
+            return oldestPerson;
+        }
+    })
+    )
 };
 
 // Do not edit below this line
