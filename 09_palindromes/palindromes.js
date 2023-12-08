c1abd39e776d548937697aaa3f80df9dbc16ba48
ilathem <isaiahlathem@gmail.com>
2023-12-07 22:24:32 -0600

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let frontIndex = 0;
+    let rearIndex = string.length - 1;
+    while (frontIndex < rearIndex) {
+        if (!(string.charAt(frontIndex).match(/[a-z0-9]/i))) {
+            frontIndex++;
+            continue;
+        }
+        if (!(string.charAt(rearIndex).match(/[a-z0-9]/i))) {
+            rearIndex--;
+            continue;
+        }
+        if (
+          !(
+            string.charAt(frontIndex).toLowerCase() ===
+            string.charAt(rearIndex).toLowerCase()
+          )
+        ) {
+            return false;
+        } else {
+            frontIndex++;
+            rearIndex--;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
